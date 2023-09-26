42291afb4bfa5ed2f1838253e654f9077a31181d
zx20student116 <zx20student116@colegiovalledelmiro.es>
2023-09-26 10:27:01 +0200

    Añadida resta

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+#!C:\Users\zx20student116\AppData\Local\Programs\Python\Python311\python.exe
+
+num1 = int(input("Escriba el primer número: "))
+num2 = int(input("Escriba el segundo número: "))
+
+def sumar():
+ print("El resultado de su suma es:")
+ print(num1+num2)
+
+def restar():
+ print("El resultado de su resta es:")
+ print(num1+num2)
+
+sumar()
+restar()
