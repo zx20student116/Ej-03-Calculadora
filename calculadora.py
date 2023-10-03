0e06585d27e2580a45a88738b17b6f02fb0eecd6
zx20student116 <zx20student116@colegiovalledelmiro.es>
2023-10-03 09:22:20 +0200

    Añadido potencias

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,7 @@
 #!C:\Users\zx20student116\AppData\Local\Programs\Python\Python311\python.exe
 
+import math
+
 num1 = int(input("Escriba el primer número: "))
 num2 = int(input("Escriba el segundo número: "))
 
@@ -16,12 +18,17 @@ def multiplicacion():
     print("El resultado de su multiplicación es:")
     print(num1 * num2)
 
+def potencia():
+    print("El resultado de su potencia es:")
+    print(int(math.pow(num1, num2)))
+
 print("Seleccione la operación:")
 print("1. Suma")
 print("2. Resta")
 print("3. Multiplicación")
+print("4. Potencia")
 
-opcion = int(input("Elija una opción (1/2/3): "))
+opcion = int(input("Elija una opción (1/2/3/4): "))
 
 if opcion == 1:
     sumar()
@@ -29,5 +36,7 @@ elif opcion == 2:
     restar()
 elif opcion == 3:
     multiplicacion()
+elif opcion == 4:
+    potencia()
 else:
-    print("Opción no válida. Por favor, elija una opción válida (1/2/3).")
+    print("Opción no válida. Por favor, elija una opción válida (1/2/3/4).")
